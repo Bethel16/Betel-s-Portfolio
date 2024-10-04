124a1e36bcf4f6856b3486a411ccbf5349434842
Bethel <beteltesfu15@gmail.com>
2024-10-04 04:49:57 +0300

    Second Commit

--- portofolio_app/static/assets/js/script.js
+++ portofolio_app/static/assets/js/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
-
     $(window).on('scroll load', function () {
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
@@ -38,8 +36,6 @@ $(document).ready(function () {
     });
 
 });
-
-
  // <!-- emailjs to mail contact form data -->
  $("#contact-form").submit(function (event) {
     emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
@@ -48,24 +44,12 @@ $(document).ready(function () {
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             document.getElementById("contact-form").reset();
-            alert("Form Submitted Successfully");
         }, function (error) {
             console.log('FAILED...', error);
-            alert("Form Submission!");
         });
     event.preventDefault();
 });
 // <!-- emailjs to mail contact form data -->
-
-document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState === "visible") {
-        document.title = "Portfolio | Betel Tesfu";
-        $("#favicon").attr("href", faviconUrl);
-    } else {
-        document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href", faviconUrl);
-    }
-});
 
 
 
